e6ce220aa3e0952a8d84511b8847d5196d9f75f5
michfit <xuehuasu@gmail.com>
2018-11-02 18:48:51 -0700

    adding app.js and other stuff

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Home from './views/Home';
+// import StudentTeacher from './views/studentTeacher/index.js';
+import {BrowserRouter, Route} from 'react-router-dom';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <BrowserRouter>
+        <div>
+          <div className="App">
+            <Home />
+          </div>
+        </div>
+      </BrowserRouter>
+      );
   }
+  
 }
 
 export default App;
