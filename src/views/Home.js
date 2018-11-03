02b2bd9226a52bc07a52ab49df1d169d484d0163
michfit <xuehuasu@gmail.com>
2018-11-02 17:09:28 -0700

    created three files in views

---
+++ src/views/Home.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { Form, FormGroup, Label, Input, FormText, Jumbotron, Container, Row, Col, Button} from 'reactstrap';
+
+export default class Home extends Component {
+	state={
+
+	}
+	render() {
+    return (
+      <div className="App">
+        <div className="Jumbotron">
+          <Jumbotron fluid>
+            <Container fluid>
+              <h1 className="display-3">Language Exchange</h1>
+                <p className="lead">This is for the language exchange project.</p> 
+            </Container>
+          </Jumbotron>
+        </div>
+
+        <Form>
+          <FormGroup>
+            <h1> I want to learn 
+            <br /><br />
+            <div className="col-md-6 offset-md-3">
+              <Input type="language" name="language" id="language" placeholder="language" />
+            </div>
+            </h1>
+            
+          </FormGroup>
+        </Form>
+
+        <div className="Button">
+          <Button color="primary">Already Learning</Button>{' '}
+        </div>
+
+      </div>
+        
+    );
+  }
+}
